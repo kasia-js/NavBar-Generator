7e1fbd69ded07cd60565c44024d6528248a2bbab
RashmiBalaji <rashmimenonv@gmail.com>
2020-09-26 12:03:46 +0200

    Work in progress on nested-menu

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,47 @@
 import React from 'react'
 
 import { ExampleComponent } from 'react-navbar-generator'
+import {BrowserRouter} from 'react-router-dom'
 import 'react-navbar-generator/dist/index.css'
 
 const App = () => {
-  const inputMenu = ["menu1","menu2","menu3", "menu4", "menu5","menu6", "menu7"]
+  const inputMenu = 
+    [
+    {
+      id: 1,
+      text: "menu1",
+      children : []
+    },
+
+    {
+      id: 2,
+      text: "menu2",
+      children: [
+        {
+          id: 2.1,
+          text: "nested2"
+        }
+      ]
+    },
+    {
+      id: 3,
+      text: "menu4",
+      children : [
+        {
+          id: 3.1,
+          text: "nested4"
+        }
+      ]
+    },
+  ]
+
+  
   return (
     <>
-    <ExampleComponent option = "horizontal" value = {inputMenu}/>
-    <ExampleComponent option = "vertical" value = {inputMenu}/>
-  
+    <BrowserRouter>
+    <ExampleComponent option = "horizontal" value = {inputMenu} nested="yes"/>
+    <ExampleComponent option = "vertical" value = {inputMenu} nested="false"/>
+    </BrowserRouter>
   </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,107 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
+
 import styles from './styles.module.css'
 
+import { NavLink } from 'react-router-dom'
+
 export const ExampleComponent = (props) => {
-  console.log(props.option)
-  const inputList = props.value.map((ele) => (
-    // eslint-disable-next-line react/jsx-key
-    <li>
-      <a href='#'>{ele}</a>
-    </li>
-  ))
+  const [isShown, setIsShown] = useState(false)
+  var nestedChildren
+
+  function onEnter(e) {
+    console.log(isShown, 'from on enter')
+    setIsShown(true)
+    const idValue = Number(e.target.id)
+    const nestedParent = props.value.find((ele) => ele.id === idValue)
+    nestedChildren = nestedParent.children.map((subEl) => {
+      console.log('inside map of children')
+      console.log(subEl.text)
+      // eslint-disable-next-line prettier/prettier
+       return (
+        <>
+          <li>{subEl.text}</li>
+        </>
+      )
+    })
+  }
+  console.log(nestedChildren)
+
+  function onLeave(e) {
+    setIsShown(false)
+  }
+
+  const inputList = props.value.map(function (ele) {
+    if (ele.children.length === 0) {
+      return (
+        <li id={ele.id} className='styles.menuItemH'>
+          {ele.text}
+        </li>
+      )
+    }
+    // eslint-disable-next-line no-unused-expressions
+    // eslint-disable-next-line no-unused-expressions
+    else
+      return (
+        <>
+          <li
+            id={ele.id}
+            className='mainEl'
+            onMouseEnter={onEnter}
+            onMouseLeave={onLeave}
+          >
+            {ele.text}
+          </li>
+
+          <ul className='subEl'>
+            {ele.children.map((subEl) => (
+              // eslint-disable-next-line react/jsx-key
+              <li>{subEl.text}</li>
+            ))}
+          </ul>
+        </>
+      )
+  })
+
+  // const nestedList = props.value
+  //   .filter((ele) => ele.children.length > 0)
+  //   .map((ele) => {
+  //     // eslint-disable-next-line react/jsx-key
+  //     // if (isShown) {
+  //     return (
+  //       <ul
+  //       // onMouseEnter={() => console.log('mouse enter')}
+  //       // onMouseLeave={() => console.log('mouse leave')}
+  //       >
+  //         {ele.children.map((subEl) => (
+  //           <li>{subEl.text}</li>
+  //         ))}
+  //       </ul>
+  //     )
+  //     // }
+  //   })
 
   return (
     <div>
       {props.option === 'horizontal' && (
-        <div className='navigationH'>
-        <ul className={styles.menuitemH}>{inputList}</ul>
+        <div>
+          <ul className={styles.menuitemH}>{inputList}</ul>
         </div>
       )}
       {props.option === 'vertical' && (
-        <div className='navigationV'>
-        <ul className={styles.menuitemV}>{inputList}</ul>
-        </div>
+        <div className={styles.menuitemV}>{nestedChildren}</div>
       )}
     </div>
   )
 }
+
+// {isShown && isNested && console.log('Nested ele to be rendered')}
+// import { NavLink, Route } from 'react-router-dom'
+// navlink from react router dom --> name of menu, path
+// in case of optional nested ele, dropdown ,
+// one level of nesting
+// some customizable options for user as width, color
+
+// <Router>
+//   <NavLink to='/'>{ele.text}</NavLink>
+//   <Route path='/' component={ele.text} />
+// </Router>
